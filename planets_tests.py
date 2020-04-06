4e8c5e7898258d6a386ffc5ca4df93e00e78f580
Marissa Leal Darnell <mdarnell@calpoly.edu>
2020-04-05 22:04:23 -0700

    These are the test functions for lab0

--- planets_tests.py
+++ planets_tests.py
@@ -11,6 +11,24 @@ class Test_planets(unittest.TestCase):
         weight_on_planets()
         #print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
+        
+    def test_02(self):
+        sys.stdin = io.StringIO("110")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 41.80 pounds.\nOn Jupiter you would weigh 257.40 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+        
+    def test_03(self):
+        sys.stdin = io.StringIO("100")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 38.00 pounds.\nOn Jupiter you would weigh 234.00 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+        
+    
 
 
 if __name__ == "__main__":
